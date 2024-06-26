f35e28a03e6847b76f6222f090ffd3e2ed202e9c
Chaerini <dlcofls21@naver.com>
2024-06-26 16:02:01 +0900

    push test

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import mysql from 'mysql2' // npm instal mysql2
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+console.log("push");
 // __filename과 __dirname을 생성
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
