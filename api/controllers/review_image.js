ef8a02f038ca6afb855af60cb7249d90608b4071
andykcal <andykcal@naver.com>
2024-06-27 15:44:00 +0900

    review_image.js 수정

--- api/controllers/review_image.js
+++ api/controllers/review_image.js
@@ -10,3 +10,23 @@ export const createImage=async(req,res)=>{
         res.status(400).json({error:error.message});
     }
 };
+export const updateImage = async(req,res) =>{
+    const {reviewId}=req.params;
+    const {image_url} = req.body;
+    console.log(req.body);
+    try{
+        const [result] = await pool.query("UPDATE review_image SET image_url=? WHERE review_id=?",[image_url,reviewId])
+        res.status(200).json({message:"수정 완료"})
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
+};
+export const deleteImage = async(req,res) =>{
+    const {reviewId} = req.params;
+    try{
+        const [result] = await pool.query("DELETE FROM review_image WHERE review_id=?",[reviewId])
+        res.status(200).json({message:"삭제 완료"})
+    }catch(error){
+        res.status(400).json({error:error.message});
+    }
+}
