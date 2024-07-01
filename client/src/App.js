ad073e25b8f41db2711e542f625bf997da8bf5d4
[Choirosie] <[choieunmi0811@gmail.com]>
2024-07-01 17:21:10 +0900

    수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { useCallback, useState } from "react";
-import {
-  BrowserRouter,
-  Route,
-  Routes
-} from "react-router-dom";
-import Cart from './pages/cart/Cart';
-import Order from './pages/order/Order';
-import OrderComplete from './pages/order/OrderComplete';
-import CartItemsProvider from './pages/cart/CartItems';
-import AddNotice from './pages/notice/AddNotice';
-import NoticeList from './pages/notice/NoticeList';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "./pages/cart/Cart";
+import Order from "./pages/order/Order";
+import OrderComplete from "./pages/order/OrderComplete";
+import CartItemsProvider from "./pages/cart/CartItems";
+import AddNotice from "./pages/notice/AddNotice";
+import NoticeList from "./pages/notice/NoticeList";
 import GetProduct from "./pages/productManage/getProduct";
-import Modal from './pages/notice/Modal';
-import Product from './pages/productManage/addProduct';
-import UpdateProduct from './pages/productManage/updateProduct';
-import GetUser from './pages/userManage/getUser';
-import Coupon from './pages/couponManage/addCoupon';
-import Login from './component/login/Login';
-import Register from './component/register/Register';
-import Profile from './pages/profile/Profile';
-import MyReview from './pages/myreview/MyReview';
-import MyCoupon from './pages/mycoupon/MyCoupon';
+import Modal from "./pages/notice/Modal";
+import Product from "./pages/productManage/addProduct";
+import UpdateProduct from "./pages/productManage/updateProduct";
+import GetUser from "./pages/userManage/getUser";
+import Coupon from "./pages/couponManage/addCoupon";
+import Login from "./component/login/Login";
+import Register from "./component/register/Register";
+import Profile from "./pages/profile/Profile";
+import MyReview from "./pages/myreview/MyReview";
+import MyCoupon from "./pages/mycoupon/MyCoupon";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/Cart' element={<CartItemsProvider><Cart /></CartItemsProvider>} />
-        <Route path='/Order' element={<CartItemsProvider><Order /></CartItemsProvider>} />
-        <Route path='/Order-Complete' element={<OrderComplete />} />
+        <Route path="/Cart" element={<CartItemsProvider><Cart /></CartItemsProvider>}/>
+        <Route path="/Order" element={ <CartItemsProvider><Order /></CartItemsProvider>}/>
+        <Route path="/Order-Complete" element={<OrderComplete />} />
         <Route path="/addnotice" element={<AddNotice />} />
         <Route path="/noticelist" element={<NoticeList />} />
         <Route path="/getproduct" element={<GetProduct />} />
-        <Route path='/addproduct' element={<Product />} />
+        <Route path="/addproduct" element={<Product />} />
         <Route path="/modal" element={<Modal />} />
-        <Route path='/updateproduct/:id' element={<UpdateProduct />} />
+        <Route path="/updateproduct/:id" element={<UpdateProduct />} />
         {/* <Route path="/" element={<Main />} /> */}
         <Route path="/getuser" element={<GetUser />} />
         <Route path="/addcoupon" element={<Coupon />} />
@@ -47,6 +43,6 @@ function App() {
         <Route path="/mycoupon" element={<MyCoupon />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 export default App;
