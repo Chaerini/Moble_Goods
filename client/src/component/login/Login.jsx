7e03026a541a99bfdf9135a2735bd8c3d9f630e8
Chaerini <dlcofls21@naver.com>
2024-07-02 11:23:29 +0900

    client/index.js 수정, 로그인 기능 추가

--- client/src/component/login/Login.jsx
+++ client/src/component/login/Login.jsx
@@ -1,15 +1,51 @@
 import './login.css';
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthContext";
+import axios from "axios";
 
 const Login = () => {
+    const [credentials, setCredentials] = useState({
+        username: undefined,
+        password: undefined,
+    });
+
+    const { loading, error, dispatch } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "LOGIN_START" });
+        try {
+            const apiUrl = process.env.REACT_APP_API_URL;
+            const res = await axios.post(`${apiUrl}/auth/login`, credentials, { withCredentials: true });
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+
+            navigate('/');
+        } catch (err) {
+            dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleClick(e);
+        }
+    }
+
     return (
         <div className="login">
             <div className="login-container">
                 <h1>로고</h1>
-                <input type='text' placeholder='아이디 입력' className='login-input'></input>
-                <input type='password' placeholder='비밀번호 입력' className='login-input'></input>
-                <button className='login-button'>로그인</button>
+                <input type='text' placeholder='아이디 입력' className='login-input' id='username' onChange={handleChange}></input>
+                <input type='password' placeholder='비밀번호 입력' className='login-input' id='password' onChange={handleChange} onKeyDown={handleKeyPress}></input>
+                <button className='login-button' disabled={loading} onClick={handleClick}>로그인</button>
                 <div className='login-div-a'>
-                    <a className='login-a'>회원가입</a>
+                    <a className='login-a' href='/register'>회원가입</a>
                     <span className='login-line'>|</span>
                     <a className='login-a'>비밀번호 찾기</a>
                 </div>
--- client/src/pages/mycoupon/MyCoupon.jsx
+++ client/src/pages/mycoupon/MyCoupon.jsx
@@ -1,8 +1,36 @@
 import './mycoupon.css';
+import { faCrown, faTicket, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MyCoupon = () => {
     return (
-        <div><h1>11111</h1></div>
+        <div className='mycoupon'>
+            <div className='mycoupon-container'>
+                <h1 className='mycoupon-title'>쿠폰</h1>
+                <div className='mycoupon-dashboard'>
+                    <div className='mycoupon-membership'>
+                        <div className='mycoupon-membership-left'>
+                            <FontAwesomeIcon icon={faCrown} className='mycoupon-icon-bronze' />
+                        </div>
+                        <div className='mycoupon-membership-right'>
+                            <p className='mycoupon-membership-p-top'>이채린님 환영합니다!</p>
+                            <p className='mycoupon-membership-p-bottom'>구매 후 쿠폰혜택을 누리세요</p>
+                            <p className='mycoupon-membership-p-bottom'>추가 주문 건수 1건 이상 SILVER 등급이 됩니다.</p>
+                        </div>
+                    </div>
+                    <div className='mycoupon-use'>
+                        <FontAwesomeIcon icon={faTicket} className='mycoupon-icon-ticket' />
+                        <p className='mycoupon-use-p-top'>사용 가능 쿠폰</p>
+                        <p className='mycoupon-use-p-bottom'><em className='mycoupon-em'>0</em>장</p>
+                    </div>
+                    <div className='mycoupon-download'>
+                        <FontAwesomeIcon icon={faArrowDown} className='mycoupon-icon-download' />
+                        <p className='mycoupon-download-p-top'>받을 수 있는 쿠폰</p>
+                        <p className='mycoupon-download-p-bottom'><em className='mycoupon-em'>0</em>장</p>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
