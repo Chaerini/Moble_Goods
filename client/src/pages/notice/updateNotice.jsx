0882f64ea28a14ad385400635695c25bf5c9e4fb
andykcal <andykcal@naver.com>
2024-07-03 14:54:48 +0900

    ddddd

--- client/src/pages/notice/updateNotice.jsx
+++ client/src/pages/notice/updateNotice.jsx
@@ -7,50 +7,22 @@ const UpdateNotice = () => {
   const [notice, setNotice] = useState({
     title: "",
     content: "",
-  });
-  const location = useLocation();
+    });
+    const location = useLocation();
   const noticeId = location.pathname.split("/")[2];
   const handleChange = (e) => {
     setNotice((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+    };
   console.log();
   const handleClick = async (e) => {
     e.preventDefault();
     try {
       await axios.put(`http://localhost:8080/api/notice/` + noticeId, notice);
     } catch (err) {
-      console.log(err);
-    }
+            console.log(err);
+        }
   };
-  return (
-    <div className="Product">
-      <h1>공지사항 수정하기</h1>
-      <div className="product">
-        <input
-          type="text"
-          onChange={handleChange}
-          placeholder="제목"
-          name="title"
-          className="product-input"
-        />
-        <input
-          type="text"
-          onChange={handleChange}
-          placeholder="내용"
-          name="content"
-          className="product-input"
-        />
-        <button onClick={handleClick}>
-          <Link to="/getnotice">수정</Link>
-        </button>
-      </div>
-    </div>
-  );
-};
-
-<<<<<<< HEAD
-}
-    return(
+return(
         <div className="Product">
             <div className="product">
             <label>공지사항 수정하기</label>
@@ -61,8 +33,4 @@ const UpdateNotice = () => {
         </div>
     )
 }
-
 export default UpdateNotice;
-=======
-export default UpdateNotice;
->>>>>>> c1864e3211b17faa58baca8dfc6617dc6f3b6302
